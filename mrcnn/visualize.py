86bd3a0a665a67ac6c25161549c94f802c310006
vassanyig <73110054+vassanyig@users.noreply.github.com>
2020-11-21 22:09:13 +0000

    Update visualize.py

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -588,7 +588,7 @@ def save_image(image, image_name, boxes, masks, class_ids, scores, class_names, 
             draw.rectangle((x1, y1, x2, y2), outline=color)
 
         # Label
-        font = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
+        font = ImageFont.truetype('/Library/Fonts/arial.ttf', 15)
         draw.text((x1, y1), "%s %f" % (label, score), (255, 255, 255), font)
 
     masked_image.save(os.path.join(save_dir, '%s.jpg' % (image_name)))
