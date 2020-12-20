3589b43e8433791cd4e32af91762af4e6729014e
valentin356 <61781236+valentin356@users.noreply.github.com>
2020-12-20 23:17:50 +0100

    Update visualize.py

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -164,6 +164,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
+    print(masked_image.shape)
     if auto_show:
         plt.show()
 
