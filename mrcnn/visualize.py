5d214c289f7f89a0c53e2a81cd96fd42a6c732be
valentin356 <61781236+valentin356@users.noreply.github.com>
2020-12-23 17:53:00 +0100

    Update visualize.py

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -81,7 +81,7 @@ def apply_mask(image, mask, color, alpha=0.5):
 
 
 def display_instances(image, boxes, masks, class_ids, class_names,
-                      scores=None, iterationNum, title="",
+                      scores=None, imageName, title="",
                       figsize=(16, 16), ax=None,
                       show_mask=True, show_bbox=True,
                       colors=None, captions=None, ):
